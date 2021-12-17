f225d1b7aec9df56a46e70d3cdeffd6d998e0d81
aiya000 <aiya000.develop@gmail.com>
2021-12-17 18:09:01 +0900

    :sparkles: Make basis

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,39 @@
+import Data.Semigroup ((<>))
 import XMonad
-import XMonad.Config.Gnome
+import XMonad.Config.Gnome (gnomeConfig)
+import XMonad.StackSet (greedyView, shift, swapDown, swapUp)
+import XMonad.Util.EZConfig (additionalKeys)
 
 main :: IO ()
-main = xmonad gnomeConfig
+main = xmonad $ gnomeConfig `additionalKeys` myKeys
+
+myKeys :: [((KeyMask, KeySym), X ())]
+myKeys =
+  keys <>
+  switchingWorkspaces <>
+  puttingWindowsToWorkspace
+  where
+    keys =
+      [ ((altMask .|. controlMask, xK_h), windows swapUp)
+      , ((altMask .|. controlMask, xK_l), windows swapDown)
+      ]
+
+    switchingWorkspaces = do
+      (numKey, workspace) <- zip [xK_1 .. xK_9] myWorkspaces
+      let keymap = (altMask .|. controlMask, numKey)
+      let switching = windows $ greedyView workspace
+      pure (keymap, switching)
+
+    puttingWindowsToWorkspace = do
+      (numKey, workspace) <- zip [xK_1 .. xK_9] myWorkspaces
+      let keymap = (superMask, numKey)
+      let putting = windows $ shift workspace
+      pure (keymap, putting)
+
+    myWorkspaces = map show [1 .. 4]
+
+altMask :: KeyMask
+altMask = mod1Mask
+
+superMask :: KeyMask
+superMask = mod4Mask
